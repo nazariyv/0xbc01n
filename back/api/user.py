0ee5106deef6d4ec8cc36548317e2aa9a6db7a8a
Nazariy Vavryk <nazariy@inbox.ru>
2020-06-27 18:25:15 +0100

    get all users not working just yet

--- back/api/user.py
+++ back/api/user.py
@@ -1,5 +1,7 @@
 import logging
 import falcon  # type: ignore
+import time
+import json
 
 from back.orm.utils import create_instance
 from back.orm.user import User as UserORM
@@ -18,10 +20,23 @@ class User:
 
         new_user = req.media
         l.debug(f"{new_user=}")
+
         # TODO: validate the new user here in the future, in fact, inside of create_instance
         user = create_instance(UserORM, new_user)
+        user.created = int(time.time())
         l.debug(f"{user=}")
 
+        # will commit to the db when flushed (automatically)
         self.session.add(user)
 
         resp.code = falcon.HTTP_200
+
+    # TODO: unprotected at all at the moment, so anyone could just query it
+    def on_get(self, req, resp):
+        all_users = []
+
+        for user in self.session.query(UserORM).order_by(UserORM.created):
+            all_users.append(user.__to_dict())
+
+        resp.body = json.dumps(all_users)
+        resp.code = 200
--- back/orm/user.py
+++ back/orm/user.py
@@ -7,6 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     addr = Column(String, primary_key=True)
+    created = Column(Integer)
     name = Column(String)
     fullname = Column(String)
     nickname = Column(String)
@@ -15,6 +16,14 @@ class User(Base):
 
     def __repr__(self):
         return f"User(addr='{self.addr}', name='{self.name}', fullname='{self.fullname}', nickname='{self.nickname}, dateOfBirth={self.dateOfBirth}, aboutMe={self.aboutMe}')"
+
+    # ! do not name anything without the leading __ other than the fields that will go into db
+    def __to_dict(self):
+        dct = dict()
+        for a in dir(self):
+            if not a.startswith("__"):
+                dct[a] = getattr(self, a)
+        return dct
 
 
 def create_table():
